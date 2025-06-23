fe8096e8bfe26b09c7264f24806724adbabc14ce
jy-hwan <jyhwan0612@gmail.com>
2025-06-23 11:40:43 +0900

    yun_day2

--- Q2_20250623/yun_day2.py
+++ Q2_20250623/yun_day2.py
@@ -12,17 +12,22 @@ class Solution:
 
 
     def yun2(self):
-        x=self.x
-        is_palindrome = True                 
-        for i in range(len(x) // 2):     
-            if x[i] != x[-1 - i]:      
-                is_palindrome = False        
-                break
-        
-        return is_palindrome
+        x = self.x
+        # 음수는 회문이 될 수 없다고 가정
+        if x < 0:
+            return False
+
+        original = x
+        reversed_num = 0
+        # 1의 자리부터 차례로 꺼내서 뒤집은 수를 만듭니다
+        while x != 0:
+            reversed_num = reversed_num * 10 + (x % 10)
+            x //= 10
+
+        return reversed_num == original
     
 def main():
-    x =int(input(''))
+    x =int(input('숫자를 입력하세요'))
     sol = Solution(x)
 
     print("String 처리 회문: ", sol.yun())
